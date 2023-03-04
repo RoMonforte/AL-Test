d1c7c263771465ec4884ec13b464f8d916f7b3e7
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-03-04 03:39:55 -0600

    swagger doc

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const ordersRouter = require('./orders.router');
 const authRouter = require('./auth.router');
+const swaggerDoc = require('./swagger.json')
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
+    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
     router.use('/products', productsRouter);
     router.use('/users', usersRouter);
     router.use('/orders', ordersRouter);
